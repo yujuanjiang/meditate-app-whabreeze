51359e887fdc74931ddcdd74c4e3f9669daa8bbc
Yujuan Jiang <yujuan.jiang@polymtl.ca>
2023-05-11 22:30:46 -0400

    Updates

---
+++ precache-manifest.b16e47cdd8fc811de041bf4d6064ab37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1283374bc7d3575b9187",
+    "url": "/meditate-app-whabreeze/static/css/main.275516f2.chunk.css"
+  },
+  {
+    "revision": "1283374bc7d3575b9187",
+    "url": "/meditate-app-whabreeze/static/js/main.081ee824.chunk.js"
+  },
+  {
+    "revision": "0ac16d3b22b73af4263a",
+    "url": "/meditate-app-whabreeze/static/js/runtime~main.86eb58db.js"
+  },
+  {
+    "revision": "a519cc427a82a015e8be",
+    "url": "/meditate-app-whabreeze/static/css/2.33f21af7.chunk.css"
+  },
+  {
+    "revision": "a519cc427a82a015e8be",
+    "url": "/meditate-app-whabreeze/static/js/2.a84145dc.chunk.js"
+  },
+  {
+    "revision": "454fcd8be0e10005eb11ccadc9b1138e",
+    "url": "/meditate-app-whabreeze/index.html"
+  }
+];
